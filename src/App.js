c4ae94900349fc323049dcd51d05c24bff0105cc
JoabPa <joabparedes.ec@gmail.com>
2023-06-20 23:17:28 -0400

    Added FormAddFriend component

--- src/App.js
+++ src/App.js
@@ -24,6 +24,7 @@ export default function App() {
     <div className='app'>
       <div className='sidebar'>
         <FriendsList />
+        <FormAddFriend />
       </div>
     </div>
   );
@@ -58,7 +59,26 @@ function Friend({ friend }) {
       {friend.balance === 0 && (
         <p className='red'>You and {friend.name} are even</p>
       )}
-      <button className='button'>Select</button>
+
+      <Button>Select</Button>
     </li>
+  );
+}
+
+function Button({ children }) {
+  return <button className='button'>{children}</button>;
+}
+
+function FormAddFriend() {
+  return (
+    <form className='form-add-friend'>
+      <label>🧑‍🤝‍🧑Friend Name</label>
+      <input type='text' />
+
+      <label>🌇Image URL</label>
+      <input type='text' />
+
+      <Button>Add</Button>
+    </form>
   );
 }
