8afcb140ad480c2fa37f573be5477283a0881c58
JoabPa <joabparedes.ec@gmail.com>
2023-06-20 23:28:18 -0400

    Added FormSplitBill component

--- src/App.js
+++ src/App.js
@@ -25,7 +25,9 @@ export default function App() {
       <div className='sidebar'>
         <FriendsList />
         <FormAddFriend />
+        <Button>Add Friend</Button>
       </div>
+      <FormSplitBill />
     </div>
   );
 }
@@ -79,6 +81,31 @@ function FormAddFriend() {
       <input type='text' />
 
       <Button>Add</Button>
+    </form>
+  );
+}
+
+function FormSplitBill() {
+  return (
+    <form className='form-split-bill'>
+      <h2>Split a bill with X</h2>
+
+      <label>💵Bill value</label>
+      <input type='text' />
+
+      <label>👲Your expense</label>
+      <input type='text' />
+
+      <label>🤵X's expense</label>
+      <input type='text' disabled />
+
+      <label>🤑 Who is paying the bill?</label>
+      <select>
+        <option value='user'>You</option>
+        <option value='friend'>X</option>
+      </select>
+
+      <Button>Split bill</Button>
     </form>
   );
 }
